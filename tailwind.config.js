b3df0e3d174c542469cba6ccce3e762334830f4b
Divye2401 <52701687+Divye2401@users.noreply.github.com>
2025-08-03 12:56:44 -0400

    Initial Commit JG

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#4f46e5",
+      },
+    },
+  },
+  plugins: [],
+};
